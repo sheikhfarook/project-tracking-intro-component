136bdec024774a469a16764ef7e8403c1b57c441
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-30 12:37:25 +0530

    added the images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Dashboard from "./components/dashboard";
 import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
       <Header />
+      {/* <Dashboard /> */}
     </div>
   );
 }
